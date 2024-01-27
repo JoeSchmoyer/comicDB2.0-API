e7dcfb51aa6814df0b60f2f169f1f509c704165d
JoeSchmoyer <josephfschmoyer@gmail.com>
2024-01-27 16:07:50 -0500

    fixed requirements.txt

--- createdb.py
+++ createdb.py
@@ -3,7 +3,8 @@ from app import app
 from models import Comic
 
 with app.app_context():
-    db.drop_all()
+    #IF you want to clear original database
+    #db.drop_all()
     db.create_all()
     comic = Comic(title="testComic", author="testAuthor", artist="testArtist")
     db.session.add(comic)
